f06cbb744ed140adcf7566db11bfe66ed2c66a24
EricaSugui <ericasugui@gmail.com>
2020-11-11 20:41:31 -0300

    testing a valid C file

--- test.c
+++ test.c
@@ -6,16 +6,16 @@
 /*   By: esuguimo <esuguimo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/11 20:26:22 by esuguimo          #+#    #+#             */
-/*   Updated: 2020/11/11 20:26:23 by esuguimo         ###   ########.fr       */
+/*   Updated: 2020/11/11 20:41:01 by esuguimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int main()
+int	main(void)
 {
 	int artifacts;
-	int rooms; 
+	int rooms;
 	int paintings;
 	int sculptures;
 
@@ -23,6 +23,5 @@ int main()
 	paintings = 16;
 	sculptures = 4;
 	artifacts = rooms * (paintings + sculptures);
-
-	return(0);
+	return (0);
 }
